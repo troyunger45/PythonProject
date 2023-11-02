cdcd05eae5388a9476033e6595868eb9e63b8df0
troyunger45 <125838792+troyunger45@users.noreply.github.com>
2023-11-02 16:22:38 -0400

    cleaned it up

--- classes.py
+++ classes.py
@@ -14,11 +14,8 @@ class Car:
         self.color = str()
         
     def EnterInfo(self):
-
         global count
-
         input = e.get()
-        
         if count == 0:
             self.model = input
         elif count == 1:
@@ -28,17 +25,14 @@ class Car:
         elif count == 3:
             self.drivetrain = input
         elif count == 4:
-            self.color = input    
-        
+            self.color = input     
         count = count + 1
-        
+        InsertText()
 
     def DisplayInfo(self):
         top = Toplevel()
-        
         labelModel = Label(top, text= self.model)
         labelModel.pack()
-        #labelModel.after(1000, lambda: labelModel.destroy())
         labelYear = Label(top, text=self.year)
         labelYear.pack()
         labelMilage = Label(top, text=self.milage)
@@ -47,26 +41,40 @@ class Car:
         labelDrivetrian.pack()
         labelColor = Label(top, text=self.color)
         labelColor.pack()
-        top.after(1000, lambda: top.destroy())
+        top.after(2000, lambda: top.destroy())
     
 
     def HealthCheck(self):
         if (2023 - self.year) >= 10 or self.milage >= 90000:
             print("Your car is getting up there in age you might want to get it looked at.")
-
         else:
             print("Your car still has a lot of life in it!")
 
+def InsertText():
+    e.delete(0, END)
+    if count == 0:
+        e.insert(0,"Enter your car's model:")
+    elif count == 1:
+        e.insert(0,"Enter your car's year:")
+    elif count == 2:
+        e.insert(0,"Enter your car's milage:")
+    elif count == 3:
+        e.insert(0,"Enter your car's drivetrain:")
+    elif count == 4:
+        e.insert(0,"Enter your car's color:")
+    else:
+        e.insert(0,"You have no more items to enter click 'Show INFO'")
+  
 def main():
     #Creates an Entry and sets it position in the window with pack.
     global e
-    e = Entry(root, width=35, borderwidth=5)
+    e = Entry(root, width=50, borderwidth=5)
     e.pack(padx=5,pady=5)
     global carOne
     carOne = Car()
     global count
-    count = 0
-    
+    count = 0  
+    InsertText()
     button =Button(root, text="Show INFO", padx=40, pady=20 ,command=carOne.DisplayInfo)
     button2 = Button(root, text="Enter INFO", padx=40, pady=20 ,command=carOne.EnterInfo)
     button2.pack()

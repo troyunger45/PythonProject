96e4e2d3e01ef75a8ec82a779682e349b14a159f
troyunger45 <125838792+troyunger45@users.noreply.github.com>
2023-11-08 21:13:36 -0500

    worked on mock gui

--- TkTxtAdvGm.py
+++ TkTxtAdvGm.py
@@ -9,10 +9,8 @@ class Game:
         menu.title("Text Adventure Game!")
         menu.geometry('500x500')
         menu.configure(bg="blue")
-
         #Calls InsertMenuWidgets
         self.InsertMenuWidgets()
-
         #Defines class attributes.
         self.health = 0
         self.xp = 0
@@ -22,11 +20,11 @@ class Game:
     def InsertMenuWidgets(self):
         custom_font =("Helvetica", 25, "bold")
         menuLabel = Label(self.menu, text="Welcome to Our\nText Adventure Game", font=custom_font, bg="cyan", fg="navy", padx= 50, pady= 50)
-        menuLabel.pack()
+        menuLabel.pack(padx=20,pady=20)
         startButton = Button(self.menu, text="Start Game!", command=self.InsertGameWidgets, padx=50, pady=25)
-        startButton.pack()
-        descriptionButton = Button(self.menu, text="Game Description." , padx=32, pady=25)
-        descriptionButton.pack()
+        startButton.pack(padx=10,pady=10)
+        descriptionButton = Button(self.menu, text="Game Description.", command=self.InsertDescriptionWidgets, padx=32, pady=25)
+        descriptionButton.pack(padx=10,pady=10)
     
     def InsertGameWidgets(self):
         #Destroys the menu window.
@@ -35,11 +33,31 @@ class Game:
         gameScreen.title("Playing the game...")
         gameScreen.geometry('500x500')
         gameScreen.configure(bg="blue")
-
         #Calling the main loop for the menu to run.
         gameScreen.mainloop()
 
-    def InsertDescriptionWidgets():
+    def InsertDescriptionWidgets(self):
+        #Destroys the menu window.
+        self.menu.destroy()
+        descriptionScreen = Tk()
+        descriptionScreen.title("Game Description")
+        descriptionScreen.geometry('500x500')
+        descriptionScreen.configure(bg="blue")
+        custom_font =("Helvetica", 15, "bold")
+        descriptionLabel = Label(descriptionScreen, text="This is an old style\ntext adventure game.", font=custom_font, bg="cyan", fg="navy", padx= 50, pady= 50)
+        descriptionLabel.pack()
+        backButton = Button(descriptionScreen,text="Back to menu.",command=lambda: self.BackToMenuFromDescription(descriptionScreen) , padx=32, pady=25)
+        backButton.pack(padx=10, pady=10)
+        #Calling the main loop for the menu to run.
+        descriptionScreen.mainloop()
+    
+    def BackToMenuFromDescription(self, descriptionScreen):
+        self.menu = Tk()
+        self.menu.title("Text Adventure Game!")
+        self.menu.geometry('500x500')
+        self.menu.configure(bg="blue")
+        self.InsertMenuWidgets()
+        descriptionScreen.destroy()
 
 #Main function.
 def main():
@@ -49,8 +67,6 @@ def main():
     gameOne = Game(menu)
     #Calling the main loop for the menu to run.
     menu.mainloop()
-
-
 
 #Calling main function.    
 main()
